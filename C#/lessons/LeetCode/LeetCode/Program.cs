07a1ea01bf4c2893e619b7495071e6b601c2dde6
abdullokhonz <abdullokhon1206@gmail.com>
2025-02-13 13:41:13 +0500

    Solved new problems from LeetCode

--- C#/lessons/LeetCode/LeetCode/Program.cs
+++ C#/lessons/LeetCode/LeetCode/Program.cs
@@ -4,9 +4,6 @@
     {
         static void Main(string[] args)
         {
-            _1_TwoSum twoSum = new _1_TwoSum();
-            int[] result = twoSum.TwoSum(new int[] { 2, 5, 7, 11, 15 }, 9);
-            Console.WriteLine($"[{result[0]}, {result[1]}]");
         }
     }
 }
