0bc755926bda92e29fe1e5d59002a48a3fdc611e
abdullokhonz <abdullokhon1206@gmail.com>
2025-02-17 18:14:03 +0500

    Solved new problems from LeetCode

---
+++ C#/lessons/LeetCode/LeetCode/20_ValidParentheses.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode
+{
+    public class _20_ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            List<char> stack = new List<char>();
+            Dictionary<char, char> mapping = new Dictionary<char, char>
+            {
+                { ')', '('},
+                { '}', '{'},
+                { ']', '['}
+            };
+
+            foreach (char item in s)
+            {
+                if (mapping.ContainsKey(item))
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    char topElement = stack[stack.Count - 1];
+                    stack.RemoveAt(stack.Count - 1);
+
+                    if (mapping[item] != topElement)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Add(item);
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
--- C#/lessons/LeetCode/LeetCode/Program.cs
+++ C#/lessons/LeetCode/LeetCode/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            _20_ValidParentheses validator = new _20_ValidParentheses();
+
+            Console.WriteLine(validator.IsValid("()"));       // True
+            Console.WriteLine(validator.IsValid("()[]{}"));   // True
+            Console.WriteLine(validator.IsValid("(]"));       // False
+            Console.WriteLine(validator.IsValid("([)]"));     // False
+            Console.WriteLine(validator.IsValid("{[]}"));     // True
         }
     }
 }
