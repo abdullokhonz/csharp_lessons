e01ae03657e657a0dd726d5a131ecc7e08267330
abdullokhonz <abdullokhon1206@gmail.com>
2025-02-25 17:46:55 +0500

    Solved new problems from LeetCode

---
+++ C#/lessons/LeetCode/LeetCode/7_ReverseInteger.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode
+{
+    public class _7_ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            bool isNegative = x < 0;
+            long absX = Math.Abs((long)x);
+            string reversedStr = new string(absX.ToString().Reverse().ToArray());
+
+            if (!long.TryParse(reversedStr, out long reversedX))
+            {
+                return 0;
+            }
+
+            reversedX = isNegative ? -reversedX : reversedX;
+
+            return (reversedX >= int.MinValue && reversedX <= int.MaxValue) ? (int)reversedX : 0;
+        }
+
+        public int Reverse1(int x)
+        {
+            int s = x > 0 ? 1 : -1;
+            long reversedX = long.Parse(new string((x * s).ToString().Reverse().ToArray())) * s;
+
+            return (reversedX >= int.MinValue && reversedX <= int.MaxValue) ? (int)reversedX : 0;
+        }
+
+        public int Reverse2(int x)
+        {
+            bool isNegative = x < 0;
+            string reversedStr = new string(Math.Abs(x).ToString().Reverse().ToArray());
+
+            if (!long.TryParse(reversedStr, out long reversedX))
+            {
+                return 0;
+            }
+
+            reversedX = isNegative ? -reversedX : reversedX;
+
+            return (reversedX >= int.MinValue && reversedX <= int.MaxValue) ? (int)reversedX : 0;
+        }
+    }
+}
---
+++ C#/lessons/LeetCode/LeetCode/IntegerToRoman.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    public class IntegerToRoman
+    {
+        public string IntToRoman(int num)
+        {
+            var roman = new (int Value, string Symbol)[]
+            {
+                (1000, "M"), (900, "CM"), (500, "D"),
+                (400, "CD"), (100, "C"), (90, "XC"),
+                (50, "L"), (40, "XL"), (10, "X"),
+                (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+            };
+
+            var result = new StringBuilder();
+
+            foreach (var (value, symbol) in roman)
+            {
+                while (num >= value)
+                {
+                    num -= value;
+                    result.Append(symbol);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
