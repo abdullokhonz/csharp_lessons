6f5c4a455d319dade8ebe12d9e5913281b15a620
abdullokhonz <abdullokhon1206@gmail.com>
2025-03-04 20:51:56 +0500

    Solved new problems from LeetCode

---
+++ C#/lessons/LeetCode/LeetCode/3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class _3_LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int start = 0, maxLength = 0;
+            var window = new HashSet<char>();
+
+            for (int end = 0; end < s.Length; end++)
+            {
+                char currentChar = s[end];
+
+                while (window.Contains(currentChar))
+                {
+                    window.Remove(s[start]);
+                    start++;
+                }
+
+                window.Add(currentChar);
+
+                maxLength = Math.Max(maxLength, end - start + 1);
+            }
+
+            return maxLength;
+        }
+    }
+}
--- C#/lessons/LeetCode/LeetCode/Program.cs
+++ C#/lessons/LeetCode/LeetCode/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            _3_LongestSubstringWithoutRepeatingCharacters test = new _3_LongestSubstringWithoutRepeatingCharacters();
+            Console.WriteLine(test.LengthOfLongestSubstring("abcabcbb"));
+            Console.WriteLine(test.LengthOfLongestSubstring("pwwkew"));
+            Console.WriteLine(test.LengthOfLongestSubstring("bbbb"));
+            Console.WriteLine(test.LengthOfLongestSubstring("au"));
+            Console.WriteLine(test.LengthOfLongestSubstring("aab"));
         }
     }
 }
