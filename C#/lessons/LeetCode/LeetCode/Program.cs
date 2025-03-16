4fac82b9a252a88d38d3439cc56be65aabd05edf
abdullokhonz <abdullokhon1206@gmail.com>
2025-03-16 15:01:14 +0500

    Solved new problems from LeetCode

--- C#/lessons/LeetCode/LeetCode/Program.cs
+++ C#/lessons/LeetCode/LeetCode/Program.cs
@@ -4,10 +4,19 @@
     {
         static void Main(string[] args)
         {
-            _6_ZigzagConversion test = new _6_ZigzagConversion();
-            Console.WriteLine(test.Convert("Abdullokhon", 3));
-            Console.WriteLine(test.Convert("PAYPALISHIRING", 3));
-            Console.WriteLine(test.Convert("PAYPALISHIRING", 4));
+            int[] nums1 = { 1, 1, 2 };
+            int k1 = new _26_RemoveDuplicatesFromSortedArray().RemoveDuplicates(nums1);
+            Console.WriteLine($"{k1}, nums = [{string.Join(", ", nums1[..k1])}, {string.Join(", ", Enumerable.Repeat("_", nums1.Length - k1))}]");
+
+            // Ожидаемый вывод:
+            // 2, nums = [1, 2, _]
+
+            int[] nums2 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int k2 = new _26_RemoveDuplicatesFromSortedArray().RemoveDuplicates(nums2);
+            Console.WriteLine($"{k2}, nums = [{string.Join(", ", nums2[..k2])}, {string.Join(", ", Enumerable.Repeat("_", nums2.Length - k2))}]");
+
+            // Ожидаемый вывод:
+            // 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
         }
     }
 }
