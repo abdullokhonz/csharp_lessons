5204380de3961b51613f20ea42f612939a89e62d
abdullokhonz <abdullokhon1206@gmail.com>
2025-04-02 17:19:04 +0500

    Solved new problems from LeetCode

---
+++ C#/lessons/LeetCode/LeetCode/35_SearchInsertPosition.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode
+{
+    public class _35_SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target)
+                    return mid;
+
+                if (nums[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            return left;
+        }
+
+        public int SearchInsert2(int[] nums, int target)
+        {
+            int index = Array.BinarySearch(nums, target);
+            return index >= 0 ? index : ~index; // ~index даёт место вставки
+        }
+    }
+}
--- C#/lessons/LeetCode/LeetCode/Program.cs
+++ C#/lessons/LeetCode/LeetCode/Program.cs
@@ -4,19 +4,10 @@
     {
         static void Main(string[] args)
         {
-            int[] nums1 = { 1, 1, 2 };
-            int k1 = new _26_RemoveDuplicatesFromSortedArray().RemoveDuplicates(nums1);
-            Console.WriteLine($"{k1}, nums = [{string.Join(", ", nums1[..k1])}, {string.Join(", ", Enumerable.Repeat("_", nums1.Length - k1))}]");
+            _35_SearchInsertPosition test = new _35_SearchInsertPosition();
 
-            // Ожидаемый вывод:
-            // 2, nums = [1, 2, _]
-
-            int[] nums2 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-            int k2 = new _26_RemoveDuplicatesFromSortedArray().RemoveDuplicates(nums2);
-            Console.WriteLine($"{k2}, nums = [{string.Join(", ", nums2[..k2])}, {string.Join(", ", Enumerable.Repeat("_", nums2.Length - k2))}]");
-
-            // Ожидаемый вывод:
-            // 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
+            Console.WriteLine(test.SearchInsert([1, 3, 5, 6], 5));
+            Console.WriteLine(test.SearchInsert([1, 3, 5, 6], 2));
         }
     }
 }
