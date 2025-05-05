a0323c6f8d5f34628feed3930cc75753eccd4668
abdullokhonz <abdullokhon1206@gmail.com>
2025-05-05 19:40:10 +0500

    Solved new problems from LeetCode

---
+++ C#/lessons/LeetCode/LeetCode/41_FirstMissingPositive.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode
+{
+    public class _41_FirstMissingPositive
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (
+                    nums[i] >= 1 &&
+                    nums[i] <= nums.Length &&
+                    nums[nums[i] - 1] != nums[i]
+                )
+                {
+                    int correctIndex = nums[i] - 1;
+                    int temp = nums[i];
+                    nums[i] = nums[correctIndex];
+                    nums[correctIndex] = temp;
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i + 1)
+                {
+                    return i + 1;
+                }
+            }
+
+            return nums.Length + 1;
+        }
+    }
+}
--- C#/lessons/LeetCode/LeetCode/Program.cs
+++ C#/lessons/LeetCode/LeetCode/Program.cs
@@ -4,10 +4,11 @@
     {
         static void Main(string[] args)
         {
-            _2962_Count_Subarrays_Where_Max_Element_Appears_At_Least_K_Times test = new _2962_Count_Subarrays_Where_Max_Element_Appears_At_Least_K_Times();
+            _41_FirstMissingPositive test= new _41_FirstMissingPositive();
 
-            Console.WriteLine(test.CountSubarrays([1, 3, 2, 3, 3], 2));
-            Console.WriteLine(test.CountSubarrays([1, 4, 2, 1], 3));
+            Console.WriteLine(test.FirstMissingPositive([1, 2, 0]));
+            Console.WriteLine(test.FirstMissingPositive([3, 4, -1, 1]));
+            Console.WriteLine(test.FirstMissingPositive([7, 8, 9, 11, 12]));
         }
     }
 }
