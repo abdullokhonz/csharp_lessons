67a49ae34e1fbf9cf7c083c25653a1822e36f4bf
abdullokhonz <abdullokhon1206@gmail.com>
2025-07-05 21:11:33 +0500

    Added folder 'Additional lessons'

---
+++ C#/lessons/ad/ad/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            UnfoldedArrayIntoNewArray test = new UnfoldedArrayIntoNewArray();
+
+            test.ReverseArray();
+        }
+    }
+}
---
+++ C#/lessons/ad/ad/UnfoldedArrayIntoNewArray.cs
@@ -0,0 +1,18 @@
+﻿namespace ad
+{
+    public class UnfoldedArrayIntoNewArray
+    {
+        public void ReverseArray()
+        {
+            //var reversed = Console.ReadLine()
+            //                  .Split()
+            //                  .Select(int.Parse)
+            //                  .Reverse()
+            //                  .ToArray();
+
+            //Console.WriteLine("Новый массив: " + string.Join(" ", reversed));
+
+            Console.WriteLine("Новый массив: " + string.Join(" ", Console.ReadLine().Split().Select(int.Parse).Reverse()));
+        }
+    }
+}
