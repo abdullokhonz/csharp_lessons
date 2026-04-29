4dc962207aa7fc8756e7b522afdbcea7c23ddce5
abdullokhonz <abdullokhon1206@gmail.com>
2026-04-29 17:16:12 +0500

    Solved new problem from gemini (The Shopping List).

---
+++ C#/lessons/problems/gemini/TheShoppingList/TheShoppingList/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TheShoppingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            List<string> purchases = new List<string>();
+
+            for (byte i = 0; i < 3; i++)
+            {
+                Console.Write($"Введите название {i + 1} купленного товара: ");
+                purchases.Add(Console.ReadLine()!);
+            }
+
+            if (purchases.Contains("хлеб"))
+                Console.WriteLine("Вы купили хлеб");
+
+            Console.WriteLine("Список купленных товаров:");
+            foreach (var item in purchases)
+            {
+                Console.WriteLine($"- {item}");
+            }
+        }
+    }
+}
