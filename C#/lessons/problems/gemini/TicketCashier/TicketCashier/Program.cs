3624259106deb55d6f37d660c7788059bd5afdf7
abdullokhonz <abdullokhon1206@gmail.com>
2026-05-06 16:04:43 +0500

    Solved new problem from gemini (Ticket Cashier).

---
+++ C#/lessons/problems/gemini/TicketCashier/TicketCashier/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace TicketCashier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int[] coins = { 1, 2, 5, 10 };
+            int amount = 13;
+            Console.WriteLine(GetMinCoins(coins, amount));
+        }
+
+        static int GetMinCoins(int[] coins, int amount)
+        {
+            Array.Sort(coins);
+            Array.Reverse(coins);
+
+            int count = 0;
+
+            foreach (int coin in coins)
+            {
+                if (amount >= coin)
+                {
+                    while (amount >= coin)
+                    {
+                        amount -= coin;
+                        count++;
+                    }
+
+                    /*
+                    if (amount == 0) break;
+
+                    count += amount / coin;
+                    amount %= coin;
+                    */
+                }
+            }
+
+            return count;
+        }
+    }
+}
