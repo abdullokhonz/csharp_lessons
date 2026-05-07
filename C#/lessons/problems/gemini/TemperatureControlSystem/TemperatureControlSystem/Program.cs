d8dd8c8a6c329ee9fa868420256580b811bbb794
abdullokhonz <abdullokhon1206@gmail.com>
2026-05-07 15:21:47 +0500

    Solved new problem from gemini (Temperature Control System).

---
+++ C#/lessons/problems/gemini/TemperatureControlSystem/TemperatureControlSystem/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TemperatureControlSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            double[] temperatures = new double[] { 20.5, 22.3, 19.8, 51.4, 23.5, 17.8, 18.9, 25.2, 26.7, 27.5 };
+
+            TemperatureControlService service = new TemperatureControlService(temperatures.ToList());
+
+            var report = service.TemperatureSensorReport();
+            service.PrintReport(report);
+        }
+    }
+}
---
+++ C#/lessons/problems/gemini/TemperatureControlSystem/TemperatureControlSystem/TemperatureControlService.cs
@@ -0,0 +1,36 @@
+﻿namespace TemperatureControlSystem
+{
+    public class TemperatureControlService
+    {
+        private readonly List<double> _temperatures;
+
+        public TemperatureControlService(List<double> temperatures)
+        {
+            _temperatures = temperatures ?? throw new ArgumentNullException(nameof(temperatures));
+        }
+
+        public (double, int, bool) TemperatureSensorReport()
+        {
+            double averageTemperature = _temperatures.Average();
+            int deviationsCount = 0;
+            bool isEmergency = false;
+
+            foreach (double temperatue in _temperatures)
+            {
+                if (temperatue < 18.0 || temperatue > 25.0) deviationsCount++;
+                if (temperatue > 50.0) isEmergency = true;
+            }
+
+            var report = (averageTemperature, deviationsCount, isEmergency);
+
+            return report;
+        }
+
+        public void PrintReport((double, int, bool) report)
+        {
+            Console.WriteLine($"Average Temperature: {report.Item1}");
+            Console.WriteLine($"Deviations Count: {report.Item2}");
+            Console.WriteLine($"Is Emergency: {report.Item3}");
+        }
+    }
+}
