4c3f1cfd72468fff7ba1c52bb995fbba89411c64
abdullokhonz <abdullokhon1206@gmail.com>
2026-05-08 15:07:08 +0500

    Solved new problem from LeetCode (2404. Most Frequent Even Element).

---
+++ C#/lessons/LeetCode/2404.MostFrequentEvenElement/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _2404.MostFrequentEvenElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine(MostFrequentEven(new int[] { 0, 1, 2, 2, 4, 4, 1 }));
+            Console.WriteLine(MostFrequentEven(new int[] { 4, 4, 4, 9, 2, 4 }));
+            Console.WriteLine(MostFrequentEven(new int[] { 29, 47, 21, 41, 13, 37, 25, 7 }));
+        }
+
+        public static int MostFrequentEven(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return -1;
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((nums[i] & 1) == 0)
+                {
+                    if (map.ContainsKey(nums[i]))
+                    {
+                        map[nums[i]]++;
+                    }
+                    else
+                    {
+                        map[nums[i]] = 1;
+                    }
+                }
+            }
+
+            if (map.Count == 0) return -1;
+
+            int maxFrequency = 0;
+            int result = -1;
+
+            foreach (var kvp in map)
+            {
+                if (kvp.Value > maxFrequency || (kvp.Value == maxFrequency && kvp.Key < result))
+                {
+                    maxFrequency = kvp.Value;
+                    result = kvp.Key;
+                }
+            }
+
+            return result;
+        }
+    }
+}
