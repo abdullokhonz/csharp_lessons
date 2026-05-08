a1c525598c3543f68e8b7f10fe645610f73b65de
abdullokhonz <abdullokhon1206@gmail.com>
2026-05-08 15:52:34 +0500

    Solved new problem from LeetCode (3158. Find the XOR of Numbers Which Appear Twice).

---
+++ C#/lessons/LeetCode/Problems/3158.FindTheXOROfNumbersWhichAppearTwice/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _3158.FindTheXOROfNumbersWhichAppearTwice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine(DuplicateNumbersXOR(new int[] { 1, 2, 1, 3 }));
+            Console.WriteLine(DuplicateNumbersXOR(new int[] { 1, 2, 3 }));
+            Console.WriteLine(DuplicateNumbersXOR(new int[] { 1, 2, 2, 1 }));
+        }
+
+        public static int DuplicateNumbersXOR(int[] nums)
+        {
+            var duplicates = nums.GroupBy(x => x)
+                                 .Where(g => g.Count() > 1)
+                                 .Select(g => g.Key)
+                                 .ToList();
+
+            int xor = 0;
+
+            foreach (int num in duplicates) xor ^= num;
+
+            return xor;
+        }
+    }
+}
